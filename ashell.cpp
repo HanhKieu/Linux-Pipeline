0ee979857d2f323abf0b969f5c033c432dfbe884
HanhKieu <hckieu@ucdavis.edu>
2016-04-03 23:58:51 -0700

    fixed it by reverting back to old branch and making changed . changed currentlineindex to currentlinesize

--- ashell.cpp
+++ ashell.cpp
@@ -50,7 +50,7 @@ int main(int argc, char *argv[]){
     struct termios SavedTermAttributes;
     char RXChar;
     char currentLine[10000];
-    int currentLineIndex = 0;
+    int currentLineSize = 0;
     vector<string> myVector;
     vector<string>::iterator it; //iterator used for insertion
     vector<string>::iterator it2; //vector iterator
@@ -69,64 +69,79 @@ int main(int argc, char *argv[]){
     while(1){
         read(STDIN_FILENO, &RXChar, 1);
 
+        //IF IT IS A PRINTABLE CHARACTER
         if(isprint(RXChar) || RXChar == 0x0A){
         	write(STDOUT_FILENO, &RXChar, 1);
-			currentLine[currentLineIndex++] = RXChar;
-		}
+			currentLine[currentLineSize++] = RXChar;
+		}//WRITE IT OUT TO THE SCREEN, THEN ADD THAT CHARACTER INTO A LIST
 
-
-        if(0x04 == RXChar){ // C-d
+        //IF CTRL+D IS TAKEN AS AN INPUT, THEN EXIT
+        if(0x04 == RXChar){
             break;
         }
-        else if(0x7F == RXChar){ //backspace
+        //IF YOU PRESS BACKSPACE
+        else if(0x7F == RXChar){
         	write(STDOUT_FILENO, deleteString.c_str(), 3);
-        	if(currentLineIndex >= 1)
-        		currentLineIndex--;
-        	currentLine[currentLineIndex] = 0;
+        	if(currentLineSize >= 1)
+        		currentLineSize--;
+        	currentLine[currentLineSize] = 0;
         }
+        //OTHERWISE
         else{
-    	    if(0x1b == RXChar){ // ' '`
+
+            //IF ' '
+    	    if(0x1b == RXChar){
+
                 read(STDIN_FILENO, &RXChar, 1);
 
-        	    if(RXChar == 0x5b){ // '['
+                //IF '['
+        	    if(RXChar == 0x5b){
         		    read(STDIN_FILENO, &RXChar, 1);
 
-        	        if(RXChar == 0x41){ // 'A' up arrow
+                    //IF UP ARROW , 'A'
+        	        if(RXChar == 0x41){
         	        	
         	        	downArrowOnce = false;
 
+                        //IF VECTOR IS EMPTY, THEN BEEP
         	        	if(myVector.empty()){
         	        		write(STDOUT_FILENO, soundString.c_str(),1);
         	        	}
         	        	else{
 
+                            //IF YOU'RE NOT AT THE END OF THE VECTOR
 		    		     	if( (it2 != myVector.end() - 1) ){
-		    		     		clearSTDOUT(it2->size());
+		    		     		clearSTDOUT(currentLineSize);
 		    		     		write(STDOUT_FILENO, it2->c_str(), it2->size());
+                                currentLineSize = it2->size();
 		    		     		++it2;
-		    		     	} //if you're not at the end icrement
+		    		     	}//THEN GO TO THE NEXT
+                            //ELSE IF YOU'VE DISPLAYED THE END ALREADY, THEN BEEP
 		    		     	else if(upArrowOnce == true){
 		    		     		write(STDOUT_FILENO, soundString.c_str(),1);
 		    		     	}
+                            //ELSE IF YOU'RE AT THE END, THEN DISPLAY IT
 		    		     	else{
-		    		     		clearSTDOUT(it2->size());
+		    		     		clearSTDOUT(currentLineSize);
 		    		     		write(STDOUT_FILENO, it2->c_str(), it2->size());
+                                currentLineSize = it2->size();
 		    		     		upArrowOnce = true;
 		    		     	}//if this is the end
 	    		     	}
                     }
-
+                    //IF DOWN ARROW 'B'
         		    else if(RXChar == 0x42){ // 'B' down arrow
-
         		    	upArrowOnce = false;
+
         		    	if(myVector.empty()){
         		    		write(STDOUT_FILENO, soundString.c_str(),1);
         		    	}
         		    	else{
-        		    		clearSTDOUT(it2->size());
+        		    		clearSTDOUT(currentLineSize);
 	        		    	if(it2 != myVector.begin()){
 	        		    		it2--;
 	        		    		write(STDOUT_FILENO, it2->c_str(), it2->size());
+                                currentLineSize = it2->size();
 	        		    	} //if your iterator is at the beginning of vector and you press down make it ding
 	        		    	else if(downArrowOnce == true){
 	        		    		write(STDOUT_FILENO, soundString.c_str(),1);
@@ -140,35 +155,41 @@ int main(int argc, char *argv[]){
         		        //printf("It was a down arrow\n");
         		    	}
                     }
-
+                    //IF C OR D MEANS LEFT OR RIGHT ARROW
                     else if(RXChar == 0x43 || RXChar == 0x44) // 'C' or 'D'
                         printf("It was a left or right arrow\n");
-
-                    else //anything else
+                    //OTHERWISE ITS SOMETHING ELSE
+                    else
                         printf("False alarm\n");
 
 		        }
 	        }
-            if(0x0a == RXChar){ //pressed enter 
-                //printf("Pressed enter\n");
-                //printf("Length of that command %d\n", currentLineIndex-1);
+
+            //IF WE PRESSED ENTER
+            if(0x0a == RXChar){
 
                	historyCounter++;
-
+                //cout << "current line size is " << currentLineSize << endl;
+                //IF WE HAVE MORE THAN 10 IN OUR HISTORY
 	        	if(historyCounter > 10){
 	        		myVector.pop_back();
 	        		historyCounter--;
-	        	}//only up to 10 in our vector
+	        	}//DELETE ONE IN OUR HISTORY, AND DECREMENT HISTORY COUNTER
 
-                currentLine[currentLineIndex-1] = 0;
-
-                string str(currentLine);
-                //printf("%s\n", str.c_str());
-                currentLineIndex = 0;
+                //REMOVES NEWLINE AT THE END
+                currentLine[currentLineSize-1] = 0;
+                currentLineSize = 0;
+            
+                //SET ITERATOR AT THE BEGINNING OF VECTOR
                 it = myVector.begin();
+
+                //INSERTS CURRENTLINE INTO VECTOR
+                string str(currentLine);
                 myVector.insert(it, str);
+
                 it2 = myVector.begin();
                 numPrevCommands++;
+
             }
         }
     }
